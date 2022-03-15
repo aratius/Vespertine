584899472e1a1901c8037b257db1b57e96067afc
aratius <arata1129matsu@icloud.com>
2022-03-16 00:55:17 +0900

    raymarchingに影を

--- src/lib/webgl/projects/fluid2D/passes/compositionPass.ts
+++ src/lib/webgl/projects/fluid2D/passes/compositionPass.ts
@@ -59,7 +59,7 @@ export default class CompositionPass implements Pass {
 
 					float seg = 50.;
 
-					vec2 p = floor(v_uv * seg)/seg;
+					vec2 p = v_uv;
 					vec2 vel = texture2D(u_color_buffer, p).xy;
 					float len = length(vel);
 
@@ -72,9 +72,7 @@ export default class CompositionPass implements Pass {
 					vec3 col2 = mix(c3, c4, vel.y);
 					vec3 col = mix(col1, col2, len);
 
-					float dots = dot(mod(v_uv * seg, 1.), len);
-
-					gl_FragColor = vec4(col * vec3(dots), 1.0);
+					gl_FragColor = vec4(col, 1.0);
 				}
 			`,
 			depthTest: false,
