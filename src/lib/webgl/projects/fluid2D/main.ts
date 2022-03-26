33bcbea28850161abdf006d4cc09df3fdf9a5c3f
aratius <arata1129matsu@icloud.com>
2022-03-26 18:49:58 +0900

    値を小さく

--- src/lib/webgl/projects/fluid2D/main.ts
+++ src/lib/webgl/projects/fluid2D/main.ts
@@ -75,7 +75,7 @@ export default class Main extends WebGLBase {
 			const a = e.alpha
 			const b = e.beta
 			const g = e.gamma
-			this._power = new Vector2(a!, b!)
+			this._power = new Vector2(b! / 30, a! / 30)
 		}, true)
 		// window.addEventListener("devicemotion", (e) => {
 		// 	this._power = new Vector2(e.acceleration!.x!, e.acceleration!.y!)
