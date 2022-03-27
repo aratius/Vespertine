6b05f07172b2c5eb77e435cd663d0114b27caac1
aratius <arata1129matsu@icloud.com>
2022-03-27 13:02:40 +0900

    値調整

--- src/lib/webgl/projects/fluid2D/main.ts
+++ src/lib/webgl/projects/fluid2D/main.ts
@@ -42,7 +42,7 @@ export default class Main extends WebGLBase {
 	}
 
 	private get _power(): Vector2 {
-		return this._powerAcc.clone().add(this._powerRot).multiplyScalar(3)
+		return this._powerAcc.clone().multiplyScalar(5).add(this._powerRot).multiplyScalar(3)
 	}
 
 	constructor(canvas: HTMLCanvasElement) {
