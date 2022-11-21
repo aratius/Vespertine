41ccb3e5e3673923690d279f6cd8fce0703eaf92
aratius <arata1129matsu@icloud.com>
2022-11-22 00:48:54 +0900

    point light

--- src/lib/webgl/projects/box/main.ts
+++ src/lib/webgl/projects/box/main.ts
@@ -1,5 +1,5 @@
 
-import { AmbientLight, BoxBufferGeometry, ConeBufferGeometry, DirectionalLight, Mesh, MeshBasicMaterial, MeshStandardMaterial, Uniform, Vector3 } from "three";
+import { AmbientLight, BoxBufferGeometry, ConeBufferGeometry, DirectionalLight, Mesh, MeshBasicMaterial, MeshStandardMaterial, SphereBufferGeometry, Uniform, Vector3 } from "three";
 import WebGLBase from "src/lib/webgl/common/main";
 import BoxMaterial from "./material/boxMaterial";
 
@@ -7,7 +7,8 @@ export default class Main extends WebGLBase {
 
 	public _projectName: string = "box";
 	private _box: Mesh<BoxBufferGeometry, BoxMaterial> | null = null;
-	private _lightPoint: Mesh | null = null;
+	private _directionalLightPoint: Mesh | null = null;
+	private _pointLightPoint: Mesh | null = null;
 
 	constructor(canvas: HTMLCanvasElement) {
 		super(canvas, {
@@ -23,18 +24,24 @@ export default class Main extends WebGLBase {
 		this._camera?.position.set(3, 3, 3);
 		this._camera?.lookAt(0, 0, 0);
 
-		this._lightPoint = new Mesh(
+		this._directionalLightPoint = new Mesh(
 			new ConeBufferGeometry(1, 2, 10, 1, false),
 			new MeshStandardMaterial({ color: 0xffffff })
 		);
-		this._lightPoint.scale.multiplyScalar(.1);
-		this._lightPoint.position.set(1, 1, 1);
-		this._lightPoint.lookAt(0, 0, 0);
-		this._lightPoint.rotateX(-Math.PI / 2);
+		this._directionalLightPoint.scale.multiplyScalar(.1);
+		this._directionalLightPoint.lookAt(0, 0, 0);
+		this._directionalLightPoint.rotateX(-Math.PI / 2);
+
+		this._pointLightPoint = new Mesh(
+			new SphereBufferGeometry(1, 10, 10),
+			new MeshBasicMaterial({ color: 0xffffff })
+		);
+		this._pointLightPoint.scale.multiplyScalar(.05);
+
 		const l = new DirectionalLight(0xffffff, .5);
 		l.position.set(1, 1, 1);
 		l.lookAt(0, 0, 0);
-		this._scene?.add(this._lightPoint, new AmbientLight(0xffffff, .4), l);
+		this._scene?.add(this._directionalLightPoint, this._pointLightPoint, new AmbientLight(0xffffff, .4), l);
 
 		this._createBox();
 	}
@@ -55,14 +62,15 @@ export default class Main extends WebGLBase {
 		this._camera?.lookAt(0, 0, 0);
 
 		const t2 = this._elapsedTime * 1;
-		this._lightPoint?.position.set(Math.sin(t2), 1, Math.cos(t2));
-		this._lightPoint?.lookAt(0, 0, 0);
-		this._lightPoint?.rotateX(-Math.PI / 2);
+		this._directionalLightPoint?.position.set(Math.sin(t2), 1, Math.cos(t2));
+		this._directionalLightPoint?.lookAt(0, 0, 0);
+		this._directionalLightPoint?.rotateX(-Math.PI / 2);
 
-		this._box!.material.uniforms.uDirectionalLightVec = new Uniform(new Vector3(0, 0, 0).sub(this._lightPoint!.position));
+		this._box!.material.uniforms.uDirectionalLightVec = new Uniform(new Vector3(0, 0, 0).sub(this._directionalLightPoint!.position));
 
 		const t3 = this._elapsedTime * 2;
-		this._box!.material.uniforms.uPointLightPos = new Uniform(new Vector3(Math.sin(t3) * .5, Math.cos(t3 * .9) * .5, Math.cos(t3) * .5));
+		this._pointLightPoint?.position.set(Math.sin(t3) * .6, Math.cos(t3 * .9) * .6, Math.cos(t3) * .6);
+		this._box!.material.uniforms.uPointLightPos = new Uniform(this._pointLightPoint?.position);
 	}
 
 	/**
