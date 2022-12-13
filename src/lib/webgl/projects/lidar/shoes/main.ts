aff9aa2c22048da717b394da752e47930eff9cb1
aratius <arata1129matsu@icloud.com>
2022-12-14 01:09:18 +0900

    morph

--- src/lib/webgl/projects/lidar/shoes/main.ts
+++ src/lib/webgl/projects/lidar/shoes/main.ts
@@ -1,5 +1,5 @@
 
-import { AmbientLight, DirectionalLight, Mesh, MeshStandardMaterial, PlaneBufferGeometry } from "three";
+import { AmbientLight, DirectionalLight, Material, Mesh, MeshStandardMaterial, PlaneBufferGeometry, Points, ShaderMaterial, Texture, Uniform } from "three";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 import WebGLBase from "src/lib/webgl/common/main";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
@@ -43,8 +43,39 @@ export default class Main extends WebGLBase {
 	private async _loadModel(): Promise<void> {
 		const model = await loadGLTF("/models/shoes.glb");
 		console.log(model);
-
-		this._scene?.add(model);
+		const mesh = model.children[0].children[0] as Mesh;
+		const p = new Points(mesh.geometry, new ShaderMaterial({
+			uniforms: {
+				uTex: new Uniform((mesh.material as MeshStandardMaterial).map),
+				uAmount: new Uniform(0)
+			},
+			vertexShader: `
+			#include <common>
+			varying vec2 vUv;
+			uniform float uAmount;
+			void main() {
+				vUv = uv;
+				vec3 offset = vec3(rand(uv)-.5, rand(uv + vec2(1.))-.5, rand(uv + vec2(2.))-.5) * uAmount;
+				gl_Position = projectionMatrix * modelViewMatrix * vec4(position + offset, 1.);
+				gl_PointSize = 20.;
+			}
+			`,
+			fragmentShader: `
+			varying vec2 vUv;
+			uniform sampler2D uTex;
+			void main() {
+				float f = length( gl_PointCoord - vec2( 0.5, 0.5 ) );
+				if ( f > 0.1 ) {
+					discard;
+				}
+				gl_FragColor = texture2D(uTex, vUv);
+			}
+			`
+		}));
+		this._scene?.add(p);
+		gsap.timeline({ repeat: -1 })
+			.to(p.material.uniforms.uAmount, { value: 1, duration: 5, ease: "expo.out" })
+			.to(p.material.uniforms.uAmount, { value: 0, duration: 2, ease: "elastic.out" });
 	}
 
 }
